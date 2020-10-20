da2488a578664f7ab97d2ac6ba16e2665f374a7d
Artem Bordetskiy <abordetskiy@gmail.com>
2020-10-20 10:32:16 -0500

    docs: comment in basic outline

--- Instructions/StarterCode/assets/js/app.js
+++ Instructions/StarterCode/assets/js/app.js
@@ -1,1 +1,13 @@
-// @TODO: YOUR CODE HERE!
+// Set up SVG
+
+
+// Pull in Data
+
+
+// Render basic chart elements (axes) 
+        //<div id="scatter"></div>
+
+// Create circles elements
+
+
+// Create event listenter
